f41e64c69a75b118173e6fe339b38b0b2d23b7df
Zain Abidin <xan.abdn@gmail.com>
2022-01-30 23:47:59 +0000

    feath third party api

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ function getInfo(){
 
 }
 
+fetch('api.openweathermap.org/data/2.5/forecast?q=london&appid=7d8f81beb3d14e46fe29586b648eabef')
+.then(Response => Response.json())
+.then(data =>{
+    for (let index = 0; index < 5; index++) {
+    
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min )+ "°";
+        
+    }
+    for (let index = 0; index < 5; index++) {
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max )+ "°";
+        
+    }
+   
+    for(i = 0; i<5; i++){
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+data.list[i].weather[0].icon+".png";
+    }
+    console.log(data)
+
+
+})
